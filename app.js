a16b2a75dfc990e49cf4eff8ff50e7aff26afce6
emmanuelgaribay.dev@gmail.com <egaribaydev@DESKTOP-01Q0V8J.localdomain>
2026-05-28 14:50:46 -0600

    correciones de codigo

--- app.js
+++ app.js
@@ -4,25 +4,23 @@ let numero3 = Number(prompt("Ingresa el tercer número:"));
 
 if (isNaN(numero1) || isNaN(numero2) || isNaN(numero3)) {
     console.log("Debes ingresar únicamente números");
-} else if (numero1 === numero2 || numero1 === numero3 || numero2 === numero3) {
-    console.log("Los números deben ser diferentes");
 } else {
     let mayor;
     let medio;
     let menor;
     
-    if (numero1 > numero2 && numero1 > numero3) {
+    if (numero1 >= numero2 && numero1 >= numero3) {
         mayor = numero1;
-        if (numero2 > numero3) {
+        if (numero2 >= numero3) {
             medio = numero2;
             menor = numero3;
         } else {
             medio = numero3;
             menor = numero2;
         }
-    } else if (numero2 > numero1 && numero2 > numero3) {
+    } else if (numero2 >= numero1 && numero2 >= numero3) {
         mayor = numero2;
-        if (numero1 > numero3) {
+        if (numero1 >= numero3) {
             medio = numero1;
             menor = numero3;
         } else {
@@ -31,7 +29,7 @@ if (isNaN(numero1) || isNaN(numero2) || isNaN(numero3)) {
         }
     } else {
         mayor = numero3;
-        if (numero1 > numero2) {
+        if (numero1 >= numero2) {
             medio = numero1;
             menor = numero2;
         } else {
@@ -43,4 +41,13 @@ if (isNaN(numero1) || isNaN(numero2) || isNaN(numero3)) {
     console.log(`Número medio: ${medio}`);
     console.log(`Número menor: ${menor}`);
     console.log(`Ordenados de mayor a menor: ${mayor}, ${medio}, ${menor}`);
-    console.log(`Ordenados de menor a mayor: ${menor}, ${medio}, ${mayor}`);}
+    console.log(`Ordenados de menor a mayor: ${menor}, ${medio}, ${mayor}`);
+
+    if (numero1 === numero2 && numero2 === numero3) {
+        console.log("Todos los números introducidos son iguales.");
+    } else if (numero1 === numero2 || numero1 === numero3 || numero2 === numero3) {
+        console.log("hay números iguales en la entrada.");
+    }
+
+
+}
